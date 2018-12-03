bad92d1cfc9836b57247c3dcd3eb31800f8c7c32
Nguyễn Thị Vân Anh <van005618@gmail.com>
2018-12-03 17:33:04 +0700

    ok

--- src/uet/oop/bomberman/entities/character/enemy/ai/AIMedium.java
+++ src/uet/oop/bomberman/entities/character/enemy/ai/AIMedium.java
@@ -15,19 +15,21 @@ public class AIMedium extends AI {
 	@Override
 	public int calculateDirection() {
 		// TODO: cài đặt thuật toán tìm đường đi
-		int i=random.nextInt(2),t;
-		if(i==0) {
+		int i=random.nextInt(5),t;
+		if(i==0||i==1) {
 			if (_bomber.getX() - _e.getX() > 0)
 				return 3;
 			else
 				return 1;
 		}
-		else {
+		else if(i==2||i==3) {
 			if (_bomber.getY() - _e.getY() > 0)
 				return 2;
 			else
 				return 0;
 		}
+		else
+			return random.nextInt(4);
 	}
 
 }
